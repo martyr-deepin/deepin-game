a3c90d933d13d27be8cb616f2f6bbe6c77b4a42c
iceleaf916 <kaisheng.ye@gmail.com>
2013-08-08 14:14:10 +0800

    add debug server ip

--- src/constant.py
+++ src/constant.py
@@ -30,5 +30,7 @@ GAME_CENTER_DBUS_PATH = '/com/deepin/game_center'
 CONFIG_DIR = os.path.join(os.path.expanduser('~'), '.config', 'deepin-game-center')
 CACHE_DIR = os.path.join(os.path.expanduser('~'), '.cache', 'deepin-game-center')
 
-GAME_CENTER_SERVER_ADDRESS = 'http://game-center.linuxdeepin.com/'
-GAME_CENTER_DATA_ADDRESS = 'http://game-center.linuxdeepin.com'
+DEBUG = True
+
+GAME_CENTER_SERVER_ADDRESS = 'http://game-center.linuxdeepin.com/' if not DEBUG else "http://59.173.241.82:11111/"
+GAME_CENTER_DATA_ADDRESS = 'http://game-center.linuxdeepin.com' if not DEBUG else "http://59.173.241.82:11111"
